252ea8b3dcc9ce4b7f2d60dab2713c602c074d10
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-07-09 15:57:34 -0400

    Make in_edge always register the effects.

--- haskell/Probability/Distribution/FakeDist.hs
+++ haskell/Probability/Distribution/FakeDist.hs
@@ -9,12 +9,13 @@ fake_sample_error = error "sampling from fake_dist is not allowed.  You can only
 fake_range_error = error "fake_dist has no range.  You can only observe it."
 
 annotated_fake_dist_0_pr tree alignment smodel sequences = do
-  in_edge "tree" tree
-  in_edge "alignment" alignment
-  in_edge "smodel" smodel
   let subst_root = modifiable (numNodes tree - 1)
 
   let (transition_ps, cls, anc_seqs, likelihood) = observe_partition_type_0 tree alignment smodel sequences subst_root
+
+  in_edge "tree" tree
+  in_edge "alignment" alignment
+  in_edge "smodel" smodel
 
   property "subst_root" subst_root
   property "transition_ps" transition_ps
@@ -27,11 +28,12 @@ fake_dist_0 tree alignment smodel =
     Distribution "fake_dist_0" (annotated_fake_dist_0_pr tree alignment smodel) (no_quantile "fake_dist_0") fake_sample_error fake_range_error
 
 annotated_fake_dist_1_pr tree smodel sequences = do
-  in_edge "tree" tree
-  in_edge "smodel" smodel
   let subst_root = modifiable (numNodes tree - 1)
 
   let (transition_ps, cls, anc_seqs, likelihood) = observe_partition_type_1 tree smodel sequences subst_root
+
+  in_edge "tree" tree
+  in_edge "smodel" smodel
 
   property "subst_root" subst_root
   property "transition_ps" transition_ps
